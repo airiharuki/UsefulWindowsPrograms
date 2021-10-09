3b89350c6f4c2e0b00da727a27539896b9b80664
EddieFromAlphaBetas <82021527+EddieFromAlphaBetas@users.noreply.github.com>
2021-10-09 13:27:07 +0700

    Add files via upload

---
+++ Ultimate tool.cmd
@@ -0,0 +1,21 @@
+@ECHO OFF
+SET choice=
+SET /p choice= Do you want to shut down this pc? [Y/N]: 
+IF NOT '%choice%'=='' SET choice=%choice:~0,1%
+IF '%choice%'=='Y' GOTO yes
+IF '%choice%'=='y' GOTO yes
+IF '%choice%'=='N' GOTO no
+IF '%choice%'=='n' GOTO no
+IF '%choice%'=='' GOTO no
+ECHO "%choice%" is not valid
+ECHO.
+GOTO start
+
+:no
+SHUTDOWN -a 
+EXIT
+
+:yes
+SHUTDOWN /s /f /t 0
+
+
