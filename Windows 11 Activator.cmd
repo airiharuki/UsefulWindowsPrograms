fc08e89e2b75cf6a0c3f497a2d5f065503358579
Edward Thong <82021527+EdwardThong@users.noreply.github.com>
2022-03-07 21:54:35 +0700

    Added Windows 11 Activator to the list!

--- Windows 11 Activator.cmd
+++ Windows 11 Activator.cmd
@@ -15,7 +15,7 @@ cscript //nologo slmgr.vbs /skms %KMS%:1688 >nul
 echo ============================================================================&echo.&echo.&cscript //nologo slmgr.vbs /ato | find /i "successfully" && (echo.&echo ============================================================================&echo.&echo #My official blog: MSGuides.com&echo.&echo #How it works: bit.ly/kms-server&echo.&echo #Please feel free to contact me at msguides.com@gmail.com if you have any questions or concerns.&echo.&echo #Please consider supporting this project: donate.msguides.com&echo #Your support is helping me keep my servers running 24/7!&echo.&echo ============================================================================&choice /n /c YN /m "Would you like to visit my blog [Y,N]?" & if errorlevel 2 exit) || (echo The connection to my KMS server failed! Trying to connect to another one... & echo Please wait... & echo. & echo. & set /a i+=1 & goto skms)
 explorer "http://github.com/EdwardThong"&goto halt
 :notsupported
-echo ============================================================================&echo.&echo Sorry, your version is not supported.&echo.&goto halt
+echo ============================================================================&echo.&echo Sorry, your version is not supported by this Activator.&echo.&goto halt
 :busy
 echo ============================================================================&echo.&echo Sorry, the server is busy and can't respond to your request. Please try again.&echo.
 :halt
