b1f1f10a3c388f2472c33eb3a79bddee08dd50ae
Edward Thong <82021527+EdwardThong@users.noreply.github.com>
2022-03-13 17:39:58 +0700

    Hulk-smashed some bugs.

--- OfflineInsiderEnroll.cmd
+++ OfflineInsiderEnroll.cmd
@@ -1,5 +1,21 @@
 @echo off
 
+:: BatchGotAdmin (Run as Admin code starts)
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+goto UACPrompt
+) else ( goto gotAdmin )
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /B
+:gotAdmin
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+pushd "%CD%"
+CD /D "%~dp0"
+:: BatchGotAdmin (Run as Admin code ends)
+
 ::Borrowed from @abbodi1406's scripts
 for /f "tokens=6 delims=[]. " %%i in ('ver') do set build=%%i
 
